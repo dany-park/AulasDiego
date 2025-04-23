ec72e3840777d6346e2ad56fd605182042349d1e
Dany Shin Park <dany.park@AGU.GOV.BR>
2025-04-23 16:53:30 -0300

    alterações para ver se da conflito

--- pythonProject/DanyDiego16/DanyDiego16.py
+++ pythonProject/DanyDiego16/DanyDiego16.py
@@ -1,7 +1,9 @@
-'''aula GITHUB configurações
+'''aula GITHUB configurações dany
 
  comando ssh-keygen gera chave ssh para adicionar a maquina na conta do github
 
 criar um repositorio vazio no github e depois...
 
- GIT push'''
+ GIT push
+Git Clone
+ GIT pull'''
