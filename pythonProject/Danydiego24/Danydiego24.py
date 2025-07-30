d232883b92be16d783d9556c5cec4685dcf117b4
Dany Shin Park <dany.park@AGU.GOV.BR>
2025-07-29 22:13:45 -0300

    29JUL

--- pythonProject/Danydiego24/Danydiego24.py
+++ pythonProject/Danydiego24/Danydiego24.py
@@ -25,13 +25,27 @@ class Playlist:
             print(musica)
         return ""
 
+    def __len__(self):
+        return len(self.musicas)
+
+    def __add__(self, outra_playlist):
+        nova = Playlist(f"{self.nome} + {outra_playlist.nome}") # mera string formatada
+        nova.musicas = self.musicas + outra_playlist.musicas
+        return nova
 
 
 musica1 = Musica("abc","pele",300)
+musica2 = Musica("eduardo e monica","legião",500)
+musica3 = Musica("faroeste caboclo","legião",450)
 playlist1 = Playlist("jingles")
 playlist1.adicionar(musica1)
 print(playlist1)
-
+playlist2 = Playlist("rock")
+playlist2.adicionar(musica2)
+playlist2.adicionar(musica3)
+print(playlist2)
+print(len(playlist2))
+print(playlist2 + playlist1)
 # --add
 # len
 # mistura de playlist
