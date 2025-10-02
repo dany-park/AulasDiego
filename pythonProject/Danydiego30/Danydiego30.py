4e619737e8913223e87fc83d9de58cd5ffe328c0
Dany Shin Park <dany.park@AGU.GOV.BR>
2025-10-02 16:28:28 -0300

    2OUT

--- pythonProject/Danydiego30/Danydiego30.py
+++ pythonProject/Danydiego30/Danydiego30.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-def pegar_nome():
+'''def pegar_nome():
     nome = input.get()
     label1.config(text=f"Seu nome é {nome}")
 #janela
@@ -22,4 +22,34 @@ botao.pack()
 label1 = tk.Label(janela,text="Seu nome é ...")
 label1.pack()
 
+janela.mainloop()'''
+
+def calcula_imc():
+    imc = float(input.get())/(float(input1.get())*float(input1.get()))
+    label2.config(text=f"Seu IMC é {imc}")
+
+
+janela = tk.Tk()
+janela.title("IMC")
+janela.geometry("500x500")
+
+label = tk.Label(janela,text="Peso(kg):", padx=100, pady=100)
+label.grid(row=0,column=0)
+
+input = tk.Entry(janela)
+input.grid(row=0,column=1)
+
+label1 = tk.Label(janela,text="Altura(metros):")
+label1.grid(row=1,column=0)
+
+input1 = tk.Entry(janela)
+input1.grid(row=1,column=1)
+
+botao = tk.Button(janela,text = "Calcular IMC",command=calcula_imc)
+botao.grid(row=3,column=1)
+
+label2 = tk.Label(janela,text="Seu IMC é ...")
+label2.grid(row=4,column=0,columnspan=2,ipady=10)
+
 janela.mainloop()
+
---
+++ pythonProject/Danydiego31/Danydiego31.py
@@ -0,0 +1,1 @@
+# tkinter - uso do GRID
